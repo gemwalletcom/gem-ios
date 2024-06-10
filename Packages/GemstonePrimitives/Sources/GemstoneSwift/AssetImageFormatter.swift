a6375886a7eefbb198363c17d69fc912f7b11d78
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-06-10 18:25:29 +0100

    Fix asset id decoding in core to properly return image_url

--- Packages/GemstonePrimitives/Sources/GemstoneSwift/AssetImageFormatter.swift
+++ Packages/GemstonePrimitives/Sources/GemstoneSwift/AssetImageFormatter.swift
@@ -9,7 +9,7 @@ public struct AssetImageFormatter {
     public init() {}
     
     public func getURL(for assetId: AssetId) -> URL? {
-        URL(string: Config.shared.imageFormatterAssetUrl(id: assetId.identifier))
+        URL(string: Config.shared.imageFormatterAssetUrl(chain: assetId.chain.rawValue, tokenId: assetId.tokenId))
     }
     
     public func getValidatorUrl(chain: Chain, id: String) -> URL {
