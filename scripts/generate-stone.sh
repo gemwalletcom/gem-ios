ca5e13ad786edd28b9ccd783912d41a0f90b4fd5
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-07-09 02:33:26 +0900

    Read IPHONEOS_DEPLOYMENT_TARGET from project.pbxproj (#103)
    * read IPHONEOS_DEPLOYMENT_TARGET from project.pbxproj
    
    * move generate logic to shell script

---
+++ scripts/generate-stone.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+CURRENT_DIR=$(dirname `realpath $0`)
+PROJ_PATH=${CURRENT_DIR}/../Gem.xcodeproj/project.pbxproj
+STONE_DIR=${CURRENT_DIR}/../core/gemstone
+PACKAGES_DIR=${CURRENT_DIR}/../Packages/Gemstone
+BUILD_MODE=$1
+
+read_deployment_target() {
+    echo -n $(/usr/libexec/PlistBuddy -c "Print" "$PROJ_PATH" | grep IPHONEOS_DEPLOYMENT_TARGET | awk -F ' = ' '{print $2}' | uniq)
+}
+
+generate() {
+    if [ -s $BUILD_MODE ]; then
+        echo "Build Gemstone debug"
+    else
+        echo "Build Gemstone $BUILD_MODE"
+    fi
+    
+    pushd ${STONE_DIR} > /dev/null
+    make apple BUILD_MODE=$BUILD_MODE IPHONEOS_DEPLOYMENT_TARGET=$(read_deployment_target)
+    rm -rf ${PACKAGES_DIR}
+    cp -Rf target/spm ${PACKAGES_DIR}
+
+    popd /dev/null
+}
+
+generate
