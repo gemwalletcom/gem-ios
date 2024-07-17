b67789a80e4e2ca2e2715781b9fb308b3cc6aed3
gemdev111 <iGemDev@gemwallet.com>
2024-07-17 18:14:42 +0300

    Round button updates
    fixes #95

--- Gem/Wallet/Scenes/WalletScene.swift
+++ Gem/Wallet/Scenes/WalletScene.swift
@@ -56,7 +56,7 @@ struct WalletScene: View {
                 ) {
                     isPresentingSelectType = $0.selectType
                 }
-                .padding(.top, 8)
+                .padding(.top, Spacing.small)
             }
             .frame(maxWidth: .infinity)
             .textCase(nil)
@@ -84,7 +84,7 @@ struct WalletScene: View {
                     }
                 )
                 .accessibilityIdentifier("manage")
-                .padding(16)
+                .padding(Spacing.medium)
                 .frame(maxWidth: .infinity, alignment: .center)
             }
         }
