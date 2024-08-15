c7b843661beff86b0c100b21946945c1c70f077d
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-08-15 06:24:03 +0100

    Add more factories to PrimitivesTestKit and improve transaction title

---
+++ Packages/Primitives/TestKit/Transaction+PrimitivesTestKit.swift
@@ -0,0 +1,34 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import Foundation
+import Primitives
+
+public extension Transaction {
+    static func mock(
+        type: TransactionType = .transfer,
+        state: TransactionState = .confirmed,
+        direction: TransactionDirection = .incoming
+    ) -> Transaction {
+        Transaction(
+            id: "1",
+            hash: "2",
+            assetId: .mock(),
+            from: "",
+            to: "",
+            contract: .none,
+            type: type,
+            state: state,
+            blockNumber: "",
+            sequence: "",
+            fee: "",
+            feeAssetId: .mock(),
+            value: "",
+            memo: .none,
+            direction: direction,
+            utxoInputs: [],
+            utxoOutputs: [],
+            metadata: .none,
+            createdAt: .now
+        )
+    }
+}
