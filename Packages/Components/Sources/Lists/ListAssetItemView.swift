01dda04c5862884ade2fe7d9f45f9799fc9e97bf
gemdev1111 <iGemDev@gemwallet.com>
2024-08-22 09:55:42 -0700

    AssetListView updates, now supported dynamic size (#141)
    * AssetListView updates, now supported dynamic size
    
    * AssetListView adjustments
    
    * ListassetItemView relability updates

---
+++ Packages/Components/Sources/Lists/ListAssetItemView.swift
@@ -0,0 +1,134 @@
+import SwiftUI
+import Style
+
+public struct ListAssetItemView: View {
+    let model: ListAssetItemViewable
+    @State private var toggleValue: Bool
+
+    public init(model: ListAssetItemViewable) {
+        self.model = model
+        _toggleValue = State(wrappedValue: model.isAssetEnabled)
+    }
+
+    public var body: some View {
+        ListItemFlexibleView(
+            left: { AssetImageView(assetImage: model.assetImage) },
+            primary: { primaryContent },
+            secondary: { secondaryContent }
+        )
+        .onChange(of: toggleValue) { _, newValue in
+            model.action?(.enabled(newValue))
+        }
+    }
+}
+
+// MARK: - Components
+
+extension ListAssetItemView {
+    @ViewBuilder
+    private var primaryContent: some View {
+        VStack(alignment: .leading, spacing: Spacing.extraSmall) {
+            headerView
+            subtitleView
+        }
+    }
+
+    private var headerView: some View {
+        HStack(spacing: Spacing.tiny) {
+            Text(model.name)
+                .textStyle(
+                    TextStyle(font: .body, color: .primary, fontWeight: .semibold)
+                )
+            if let symbol = model.symbol {
+                Text(symbol)
+                    .textStyle(.calloutSecondary)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var subtitleView: some View {
+        switch model.subtitleView {
+        case .price(let price, let priceChangePercentage24h):
+            HStack(spacing: Spacing.extraSmall) {
+                Text(price.text)
+                    .textStyle(price.style)
+                Text(priceChangePercentage24h.text)
+                    .textStyle(priceChangePercentage24h.style)
+            }
+        case .type(let textValue):
+            Text(textValue.text)
+                .textStyle(textValue.style)
+        case .none:
+            EmptyView()
+        }
+    }
+
+    @ViewBuilder
+    private var secondaryContent: some View {
+        switch model.rightView {
+        case .balance(let balance, let totalFiat):
+            balanceView(balance: balance, totalFiat: totalFiat)
+        case .toggle:
+            Toggle("", isOn: $toggleValue)
+                .labelsHidden()
+                .toggleStyle(AppToggleStyle())
+        case .copy:
+            ListButton(
+                image: Image(systemName: SystemImage.copy),
+                padding: Spacing.small,
+                action: {
+                    model.action?(.copy)
+                }
+            )
+            .background(Colors.grayVeryLight)
+            .foregroundStyle(Colors.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 24))
+        }
+    }
+
+    private func balanceView(balance: TextValue, totalFiat: TextValue) -> some View {
+        VStack(alignment: .trailing, spacing: Spacing.extraSmall) {
+            Text(balance.text)
+                .textStyle(balance.style)
+            if !totalFiat.text.isEmpty {
+                Text(totalFiat.text)
+                    .textStyle(totalFiat.style)
+            }
+        }
+        .lineLimit(1)
+    }
+}
+
+// MARK: - Previews
+
+#Preview {
+    struct AssetListViewPreviewable: ListAssetItemViewable {
+        let name: String
+        let symbol: String?
+        let assetImage: AssetImage
+        let subtitleView: ListAssetItemSubtitleView
+        let rightView: ListAssetItemRightView
+        var action: ((ListAssetItemAction) -> Void)?
+    }
+
+    return List {
+        ListAssetItemView(
+            model: AssetListViewPreviewable(
+                name: "Bitcoin",
+                symbol: "BTC",
+                assetImage: AssetImage(
+                    type: "ERC20",
+                    imageURL: URL(string: "https://assets.gemwallet.com/blockchains/bitcoin/info/logo.png")!,
+                    placeholder: .none,
+                    chainPlaceholder: .none
+                ),
+                subtitleView: .none,
+                rightView: .balance(
+                    balance: TextValue(text: "test", style: TextStyle(font: .title, color: .accentColor)),
+                    totalFiat: TextValue(text: "test2", style: TextStyle(font: .title, color: .accentColor))
+                )
+            )
+        )
+    }
+}
---
+++ Packages/Components/Sources/Lists/ListItemFlexibleView.swift
@@ -0,0 +1,39 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import SwiftUI
+import Style
+
+public struct ListItemFlexibleView<LeftView: View, PrimaryView: View, SecondaryView: View>: View {
+    let left: LeftView
+    let primary: PrimaryView
+    let secondary: SecondaryView
+
+    public init(
+        @ViewBuilder left: () -> LeftView,
+        @ViewBuilder primary: () -> PrimaryView,
+        @ViewBuilder secondary: () -> SecondaryView
+    ) {
+        self.left = left()
+        self.primary = primary()
+        self.secondary = secondary()
+    }
+
+    public var body: some View {
+        HStack(spacing: Spacing.small) {
+            left
+            HStack {
+                primary
+                Spacer(minLength: Spacing.extraSmall)
+                secondary
+            }
+        }
+    }
+}
+
+#Preview {
+    return ListItemFlexibleView(
+        left:  { Circle().foregroundStyle(.red).frame(width: 30, height: 30) },
+        primary: { Text("Primary") },
+        secondary: { Text("Secondary") }
+    )
+}
