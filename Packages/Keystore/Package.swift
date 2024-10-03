b75b5635a951282b93f03efe20c7c5c45163c1c5
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-10-03 15:16:45 +0900

    Fix swift 6 for Keystore package

--- Packages/Keystore/Package.swift
+++ Packages/Keystore/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,7 +32,10 @@ let package = Package(
                 .product(name: "SwiftProtobuf", package: "WalletCore"),
                 .product(name: "KeychainAccess", package: "KeychainAccess"),
             ],
-            path: "Sources"
+            path: "Sources",
+            swiftSettings: [
+                .swiftLanguageMode(.v6)
+            ]
         ),
         .target(
             name: "KeystoreTestKit",
