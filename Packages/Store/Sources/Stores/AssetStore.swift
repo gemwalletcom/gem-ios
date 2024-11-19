b503d9cecd8c46384fc86c636dff0778bdf8523c
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-11-19 23:15:30 +0000

    Update isSwapable

--- Packages/Store/Sources/Stores/AssetStore.swift
+++ Packages/Store/Sources/Stores/AssetStore.swift
@@ -102,7 +102,7 @@ public struct AssetStore: Sendable {
                     Columns.Asset.rank.set(to: details.isBuyable),
                     Columns.Asset.isBuyable.set(to: details.isBuyable),
                     Columns.Asset.isSellable.set(to: details.isSellable),
-                    //Columns.Asset.isSwappable.set(to: details.isSwapable), //TODO: no longer defined by the backend
+                    Columns.Asset.isSwappable.set(to: details.isSwapable),
                     Columns.Asset.isStakeable.set(to: details.isStakeable),
                 ])
             }
