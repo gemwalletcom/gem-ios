9c5138d1babef2d503f2c045a4d190e7411e90b4
gemdev111 <iGemDev@gemwallet.com>
2024-12-18 21:48:42 +0200

    added tests

--- Features/Currency/Tests/CurrencyTests/CurrencyTests.swift
+++
@@ -1,8 +0,0 @@
-// Copyright (c). Gem Wallet. All rights reserved.
-
-import Testing
-@testable import Currency
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
