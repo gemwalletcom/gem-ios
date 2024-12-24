9923da62717efd5509ea4dd2f88b5ab970b4a723
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-12-24 16:08:07 +0700

    Enable Across (#303)
    * enable across for eth
    
    * Update core
    
    * Update core
    
    * Update project.pbxproj
    
    * Update core

--- Features/Swap/Sources/Errors/ErrorWrapper+Swap.swift
+++ Features/Swap/Sources/Errors/ErrorWrapper+Swap.swift
@@ -6,11 +6,11 @@ import Localization
 
 public struct ErrorWrapper: Error, LocalizedError {
     private let error: Error
-    
+
     public init(_ error: Error) {
         self.error = error
     }
-    
+
     public var errorDescription: String? {
         switch error {
         case let swapperError as Gemstone.SwapperError:
@@ -20,12 +20,12 @@ public struct ErrorWrapper: Error, LocalizedError {
             case .NoQuoteAvailable: Localized.Errors.Swap.noQuoteAvailable
             case .NotSupportedPair, .NoAvailableProvider: Localized.Errors.Swap.notSupportedPair
             case .InvalidAddress,
-                .InvalidAmount,
-                .NetworkError,
-                .AbiError,
-                .NotImplemented,
-                .ComputeQuoteError,
-                .InvalidRoute: error.localizedDescription
+                 .InvalidAmount,
+                 .NetworkError,
+                 .AbiError,
+                 .NotImplemented,
+                 .ComputeQuoteError,
+                 .InvalidRoute: error.localizedDescription
             }
         default: error.localizedDescription
         }
