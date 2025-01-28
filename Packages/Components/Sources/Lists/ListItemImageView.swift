2dab28902d4da70d01e08da48e6cc151df68cfed
Radmir <gem@Gem-MacBook-Pro.local>
2025-01-28 09:55:01 +0500

    ListItemImageView component has been implemented (#357)

---
+++ Packages/Components/Sources/Lists/ListItemImageView.swift
@@ -0,0 +1,46 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import Foundation
+import SwiftUI
+import Style
+
+public struct ListItemImageView: View {
+    public let title: String
+    public let subtitle: String
+    public let assetImage: AssetImage?
+    public let imageSize: CGFloat
+    
+    public init(
+        title: String,
+        subtitle: String,
+        assetImage: AssetImage?,
+        imageSize: CGFloat = Sizing.list.image
+    ) {
+        self.title = title
+        self.subtitle = subtitle
+        self.assetImage = assetImage
+        self.imageSize = imageSize
+    }
+    
+    public var body: some View {
+        HStack {
+            ListItemView(title: title, subtitle: subtitle)
+            if let assetImage {
+                AssetImageView(assetImage: assetImage, size: imageSize)
+            }
+        }
+    }
+}
+
+#Preview {
+    ListItemImageView(
+        title: "Title",
+        subtitle: "Subtitle",
+        assetImage: .resourceImage(image: "logo")
+    )
+    ListItemImageView(
+        title: "Title",
+        subtitle: "Subtitle",
+        assetImage: nil
+    )
+}
