edbdf44045ea270e1df91e55ebf922e96e92e5f9
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-02-25 22:44:12 +0500

    WalletConnectorServiceError LocalizedError (#450)
    * LocalizedError has been removed
    
    * Localized WalletConnectorServiceError

---
+++ Features/WalletConnector/Sources/WalletConnector/Types/WalletConnectorServiceError+Localized.swift
@@ -0,0 +1,22 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import Foundation
+import WalletConnectorService
+import Localization
+
+extension WalletConnectorServiceError: @retroactive LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .unresolvedMethod:
+            Localized.Errors.Connections.unsupportedMethod
+        case .unresolvedChainId:
+            Localized.Errors.Connections.unsupportedChain
+        case .walletsUnsupported:
+            Localized.Errors.Connections.noSupportedWallets
+        case .wrongSignParameters:
+            Localized.Errors.Connections.invalidSignParameters
+        case .wrongSendParameters:
+            Localized.Errors.Connections.invalidSendParameters
+        }
+    }
+}
