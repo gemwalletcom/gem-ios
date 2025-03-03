b3d925b0797e511f98c35d1e71a40009d82c57e6
gemdev111 <iGemDev@gemwallet.com>
2025-03-03 14:55:41 +0200

    Fixes swapQuote not refreshed corretly, the values from/to remained old

--- Gem/Swap/ViewModels/SwapViewModel.swift
+++ Gem/Swap/ViewModels/SwapViewModel.swift
@@ -270,12 +270,8 @@ extension SwapViewModel {
 
             await MainActor.run {
                 swapState.availability = .loaded(SwapAvailabilityResult(quotes: swapQuotes))
-                if let selectedSwapQuote {
-                    onSelectQuote(selectedSwapQuote, asset: toAsset)
-                } else if let bestQuote = swapQuotes.first {
-                    onSelectQuote(bestQuote, asset: toAsset)
-                    selectedSwapQuote = bestQuote
-                }
+                selectedSwapQuote = swapQuotes.first(where: { $0 == selectedSwapQuote }) ?? swapQuotes.first
+                selectedSwapQuote.map { onSelectQuote($0, asset: toAsset) }
             }
         } catch {
             await MainActor.run { [self] in
