3ca4b9bbbb1f4bbd63f7622d2468562b8696ba3b
gemdev111 <iGemDev@gemwallet.com>
2025-03-16 17:22:17 +0200

    EmptyConentType removed Primitives dependency, which not a part of components

--- Packages/Components/Sources/Types/EmptyContentType.swift
+++ Packages/Components/Sources/Types/EmptyContentType.swift
@@ -1,7 +1,6 @@
 // Copyright (c). Gem Wallet. All rights reserved.
 
 import Foundation
-import Primitives
 
 public enum EmptyContentType {
     public enum SearchType {
@@ -13,8 +12,8 @@ public enum EmptyContentType {
     case nfts
     case priceAlerts
     case asset(symbol: String)
-    case activity(receive: VoidAction = nil, buy: VoidAction = nil)
+    case activity(receive: (() -> Void)? = nil, buy: (() -> Void)? = nil)
     case stake(symbol: String)
     case walletConnect
-    case search(type: SearchType, action: VoidAction = nil)
+    case search(type: SearchType, action: (() -> Void)? = nil)
 }
