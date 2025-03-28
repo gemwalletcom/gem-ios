17570f347bd25e4e2fbe6e0e3b3d374d8a420cc1
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-03-28 12:19:13 +0500

    minHeight instead height

--- Packages/Components/Sources/Lists/ListAssetItemView.swift
+++ Packages/Components/Sources/Lists/ListAssetItemView.swift
@@ -16,7 +16,7 @@ public struct ListAssetItemView: View {
             primary: { primaryContent },
             secondary: { secondaryContent }
         )
-        .frame(height: Spacing.list.rowHeight)
+        .frame(minHeight: Spacing.list.rowHeight)
         .onChange(of: toggleValue) { _, newValue in
             model.action?(.switcher(enabled: newValue))
         }
