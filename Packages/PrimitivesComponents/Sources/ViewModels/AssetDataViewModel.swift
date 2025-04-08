36c25b6580871046f2c025eb4a068a720472fc28
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-04-08 20:15:23 +0100

    Add Sendable conformance to view model structs

--- Packages/PrimitivesComponents/Sources/ViewModels/AssetDataViewModel.swift
+++ Packages/PrimitivesComponents/Sources/ViewModels/AssetDataViewModel.swift
@@ -6,7 +6,7 @@ import SwiftUI
 import Style
 import Components
 
-public struct AssetDataViewModel {
+public struct AssetDataViewModel: Sendable {
     private let assetData: AssetData
     private let balanceViewModel: BalanceViewModel
 
