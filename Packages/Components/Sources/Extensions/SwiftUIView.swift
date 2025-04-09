04155bdb124f15c500757346894cc4b49a18b168
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-04-09 21:04:59 +0500

    Added a Alignment

--- Packages/Components/Sources/Extensions/SwiftUIView.swift
+++ Packages/Components/Sources/Extensions/SwiftUIView.swift
@@ -150,7 +150,7 @@ public extension View {
 // MARK: - Syntactic sugar
 
 extension View {
-    func frame(size: CGFloat) -> some View {
-        frame(width: size, height: size)
+    func frame(size: CGFloat, alignment: Alignment = .center) -> some View {
+        frame(width: size, height: size, alignment: alignment)
     }
 }
