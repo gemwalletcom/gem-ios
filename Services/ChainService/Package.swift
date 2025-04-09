46349de187a941ae7d0c8b89967f20d4a4815365
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-04-09 17:38:01 +0500

    Add the necessary mocks

--- Services/ChainService/Package.swift
+++ Services/ChainService/Package.swift
@@ -11,7 +11,12 @@ let package = Package(
     products: [
         .library(
             name: "ChainService",
-            targets: ["ChainService"]),
+            targets: ["ChainService"]
+        ),
+        .library(
+            name: "ChainServiceTestKit",
+            targets: ["ChainServiceTestKit"]
+        ),
     ],
     dependencies: [
         .package(name: "Primitives", path: "../../Packages/Primitives"),
@@ -25,6 +30,14 @@ let package = Package(
                 "Blockchain",
             ],
             path: "Sources"
+        ),
+        .target(
+            name: "ChainServiceTestKit",
+            dependencies: [
+                "Primitives",
+                "Blockchain",
+            ],
+            path: "TestKit"
         ),
         .testTarget(
             name: "ChainServiceTests",
