8bc0e862037eef1f060f9ecff08c619015b92bcb
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-04-11 16:11:25 +0500

    AddTokenScene switched to in-app Safari

--- Gem/Assets/Scenes/AddTokenScene.swift
+++ Gem/Assets/Scenes/AddTokenScene.swift
@@ -102,7 +102,9 @@ extension AddTokenScene {
                 }
                 if let url = asset.explorerUrl, let text = asset.explorerText {
                     Section {
-                        NavigationOpenLink(url: url, with: ListItemView(title: text))
+                        SafariNavigationLink(url: url) {
+                            ListItemView(title: text)
+                        }
                     }
                 }
             case .error(let error):
