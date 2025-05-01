97649d6f5a8ef7b15d6569dfe25f335c5d78fb91
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-05-01 15:56:32 +0500

    AssetStoreMock implementation

--- Packages/Store/TestKit/AssetStore+TestKit.swift
+++ Packages/Store/TestKit/AssetStore+TestKit.swift
@@ -2,9 +2,63 @@
 
 import Foundation
 import Store
+import Primitives
 
-public extension AssetStore {
-    static func mock(db: DB) -> Self {
-        AssetStore(db: db)
+public final class AssetStoreMock: AssetStore, @unchecked Sendable {
+    public var addedAssets: [AssetBasic] = []
+    public var addedSearches: [(String, [AssetBasic])] = []
+    public var returnedAssets: [Asset] = []
+    public var returnedAssetsByIds: [String: Asset] = [:]
+    public var buyableUpdated: [(ids: [String], value: Bool)] = []
+    public var sellableUpdated: [(ids: [String], value: Bool)] = []
+    public var swappableUpdated: [(ids: [String], value: Bool)] = []
+    public var stakeableUpdated: [(ids: [String], value: Bool)] = []
+    public var clearedTokensCount: Int = 0
+    public var updatedLinks: [(AssetId, [AssetLink])] = []
+
+    public init() {}
+
+    public func add(assets: [AssetBasic]) throws {
+        addedAssets.append(contentsOf: assets)
+    }
+
+    public func addAssetsSearch(query: String, assets: [AssetBasic]) throws {
+        addedSearches.append((query, assets))
+    }
+
+    public func getAssets() throws -> [Asset] {
+        returnedAssets
+    }
+
+    public func getAssets(for assetIds: [String]) throws -> [Asset] {
+        assetIds.compactMap { returnedAssetsByIds[$0] }
+    }
+
+    public func setAssetIsBuyable(for assetIds: [String], value: Bool) throws -> Int {
+        buyableUpdated.append((assetIds, value))
+        return assetIds.count
+    }
+
+    public func setAssetIsSellable(for assetIds: [String], value: Bool) throws -> Int {
+        sellableUpdated.append((assetIds, value))
+        return assetIds.count
+    }
+
+    public func setAssetIsSwappable(for assetIds: [String], value: Bool) throws -> Int {
+        swappableUpdated.append((assetIds, value))
+        return assetIds.count
+    }
+
+    public func setAssetIsStakeable(for assetIds: [String], value: Bool) throws -> Int {
+        stakeableUpdated.append((assetIds, value))
+        return assetIds.count
+    }
+
+    public func clearTokens() throws -> Int {
+        clearedTokensCount
+    }
+
+    public func updateLinks(assetId: AssetId, _ links: [AssetLink]) throws {
+        updatedLinks.append((assetId, links))
     }
 }
