84ef3a5dd003e63cbc1ef318cd5212c34ebc74d9
gemdev111 <iGemDev@gemwallet.com>
2025-05-06 21:12:12 +0300

    WebSocket adjustments wrapped to io thread

--- Services/WalletConnectorService/Sources/NativeWebSocketClient.swift
+++ Services/WalletConnectorService/Sources/NativeWebSocketClient.swift
@@ -3,25 +3,19 @@
 import Foundation
 @preconcurrency import WalletConnectRelay
 
-private struct WSConnectingCompletionBox: @unchecked Sendable {
-    let callback: (() -> Void)?
-
-    func call() {
-        callback?()
-    }
-}
-
 // TODO: - remove preconcurrency, once walletconnect will be migrated to swift6
 // URLSessionWebSocketTask - use async await instead of closure, once WC will be migrated to swift6
 
 public final class NativeWebSocketClient: WebSocketConnecting, @unchecked Sendable {
-    public private(set) var isConnected = false
+    public var isConnected: Bool { io.sync { _isConnected } }
     public var onConnect: (() -> Void)?
     public var onDisconnect: ((Error?) -> Void)?
     public var onText: ((String) -> Void)?
     public var request: URLRequest
 
+    private var _isConnected = false
     private let session: URLSession
+    private let io = DispatchQueue(label: "NativeWebSocketClient")
     private var socket: URLSessionWebSocketTask?
 
     public init(request: URLRequest, session: URLSession = .shared) {
@@ -30,48 +24,68 @@ public final class NativeWebSocketClient: WebSocketConnecting, @unchecked Sendab
     }
 
     public func connect() {
-        socket = session.webSocketTask(with: request)
-        socket?.resume()
-        isConnected = true
-        onConnect?()
-        receiveNext()
+        io.async { @Sendable [weak self] in
+            guard let self else { return }
+            let task = self.session.webSocketTask(with: self.request)
+            self.socket = task
+            task.resume()
+            self._isConnected = true
+            Task { @MainActor in self.onConnect?() }
+            self.receiveNext()
+        }
     }
 
     public func disconnect() {
-        socket?.cancel(with: .goingAway, reason: nil)
-        isConnected = false
-        onDisconnect?(nil)
+        io.async { @Sendable [weak self] in
+            guard let self, let socket = self.socket else { return }
+            socket.cancel(with: .goingAway, reason: nil)
+            self.socket = nil
+            self._isConnected = false
+            Task { @MainActor in self.onDisconnect?(nil) }
+        }
     }
 
     public func write(string: String, completion: (() -> Void)? = nil) {
-        // Wrap the non-Sendable closure
-        let box = WSConnectingCompletionBox(callback: completion)
+        io.async { [weak self] in
+            guard let self,
+                  let socket = self.socket else { return }
 
-        socket?.send(.string(string)) { [weak self] error in
-            if let error {
-                self?.isConnected = false
-                self?.onDisconnect?(error)
-            } else {
-                box.call()
+            socket.send(.string(string)) { [weak self] error in
+                self?.io.async { [weak self] in
+                    guard let self else { return }
+
+                    if let error {
+                        self._isConnected = false
+                        if let onDisconnect = self.onDisconnect {
+                            Task { @MainActor in onDisconnect(error) }
+                        }
+                    } else {
+                        // Fire completion on the main thread
+                        Task { @MainActor in completion?() }
+                    }
+                }
             }
         }
     }
 
     private func receiveNext() {
-        socket?.receive { @Sendable [weak self] result in
+        guard let socket = socket else { return }
+        socket.receive { @Sendable [weak self] result in
             guard let self else { return }
             switch result {
             case .success(.string(let text)):
-                self.onText?(text)
+                Task { @MainActor in self.onText?(text) }
                 self.receiveNext()
             case .success(.data(let data)):
                 if let text = String(data: data, encoding: .utf8) {
-                    self.onText?(text)
+                    Task { @MainActor in self.onText?(text) }
                 }
                 self.receiveNext()
             case .failure(let error):
-                self.isConnected = false
-                self.onDisconnect?(error)
+                self.io.async {
+                    self._isConnected = false
+                    Task { @MainActor in self.onDisconnect?(error) }
+                }
             @unknown default:
                 break
             }
