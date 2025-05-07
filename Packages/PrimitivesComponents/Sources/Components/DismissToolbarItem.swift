8b3f34839f73d24a018837d87d5b8793653d2198
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-05-07 15:15:47 +0500

    Added reusable DismissToolbarItem

---
+++ Packages/PrimitivesComponents/Sources/Components/DismissToolbarItem.swift
@@ -0,0 +1,41 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import SwiftUI
+import Localization
+
+public struct DismissToolbarItem: ToolbarContent {
+    @Environment(\.dismiss) private var dismiss
+
+    public enum Title {
+        case cancel
+        case done
+        case custom(String)
+        
+        var localized: String {
+            switch self {
+            case .cancel: Localized.Common.cancel
+            case .done: Localized.Common.done
+            case .custom(let title): title
+            }
+        }
+    }
+
+    let title: Title
+    let placement: ToolbarItemPlacement
+    
+    public init(
+        title: Title,
+        placement: ToolbarItemPlacement
+    ) {
+        self.title = title
+        self.placement = placement
+    }
+
+    public var body: some ToolbarContent {
+        ToolbarItem(placement: placement) {
+            Button(title.localized, action: { dismiss() })
+                .bold()
+                .buttonStyle(.plain)
+        }
+    }
+}
