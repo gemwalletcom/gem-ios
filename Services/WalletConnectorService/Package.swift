5468164a21d980018928f53c05833393c0c02581
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-05-12 18:57:06 +0000

    Update reown-swift

--- Services/WalletConnectorService/Package.swift
+++ Services/WalletConnectorService/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Primitives", path: "../../Packages/Primitives"),
-        .package(url: "https://github.com/gemwalletcom/reown-swift.git", revision: "8d5bf14"),
+        .package(url: "https://github.com/gemwalletcom/reown-swift.git", revision: "dcf6e84"),
         .package(url: "https://github.com/daltoniam/Starscream.git", exact: Version(stringLiteral: "3.1.2")),
         .package(name: "Gemstone", path: "../../Packages/Gemstone"),
         .package(name: "GemstonePrimitives", path: "../../Packages/GemstonePrimitives"),
