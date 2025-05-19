3a85cb6a598587826bf57eddbc1c9f44de0b2e39
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-05-19 10:01:29 +0500

    Added clarifying comment

--- Packages/Store/TestKit/UpdateBalance+TestKit.swift
+++ Packages/Store/TestKit/UpdateBalance+TestKit.swift
@@ -8,6 +8,7 @@ import PrimitivesTestKit
 public extension Array where Element == UpdateBalance {
     static func mock(assets: [AssetBasic] = .mock()) -> Self {
         assets.enumerated().compactMap { index, asset in
+            // skip the first asset to avoid having all mocks with a balance
             guard index > 0 else { return nil }
             return UpdateBalance(
                 assetID: asset.asset.id.identifier,
