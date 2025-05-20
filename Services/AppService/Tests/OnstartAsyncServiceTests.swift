21f5f5271ba85e95dd85e2b9739c26fbdd1112b9
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-05-20 14:22:07 +0500

    OnstartAsyncServiceTests improvements

--- Services/AppService/Tests/OnstartAsyncServiceTests.swift
+++ Services/AppService/Tests/OnstartAsyncServiceTests.swift
@@ -8,20 +8,11 @@ import GemAPITestKit
 
 @testable import AppService
 
+@MainActor
 struct OnstartAsyncServiceTests {
     @Test
-    @MainActor
     func testNewRelease() async throws {
-        let service = OnstartAsyncService(
-            assetStore: .mock(),
-            nodeStore: .mock(),
-            preferences: .mock(),
-            assetsService: .mock(),
-            deviceService: .mock(),
-            bannerSetupService: .mock(),
-            configService: GemAPIConfigServiceMock(config: .mock()),
-            releaseVersionNumber: "1.0.0"
-        )
+        let service = OnstartAsyncService.mock()
         
         await confirmation(expectedCount: 1) { confirmation in
             service.releaseAction = { release in
@@ -33,9 +24,28 @@ struct OnstartAsyncServiceTests {
     }
     
     @Test
-    @MainActor
     func testSkipRelease() async throws {
-        let service = OnstartAsyncService(
+        let service = OnstartAsyncService.mock()
+        
+        await confirmation(expectedCount: 0) { confirmation in
+            service.releaseAction = { _ in
+                confirmation()
+            }
+            service.skipRelease("1.1.1")
+            await service.migrations()
+        }
+    }
+    
+    @Test
+    func testGetRelease() async throws {
+        let service = OnstartAsyncService.mock()
+        #expect(try await service.getRelease()?.version == "1.1.1")
+    }
+}
+
+extension OnstartAsyncService {
+    static func mock() -> OnstartAsyncService {
+        OnstartAsyncService(
             assetStore: .mock(),
             nodeStore: .mock(),
             preferences: .mock(),
@@ -45,13 +55,5 @@ struct OnstartAsyncServiceTests {
             configService: GemAPIConfigServiceMock(config: .mock()),
             releaseVersionNumber: "1.0.0"
         )
-        
-        await confirmation(expectedCount: 0) { confirmation in
-            service.releaseAction = { _ in
-                confirmation()
-            }
-            service.skipRelease("1.1.1")
-            await service.migrations()
-        }
     }
 }
