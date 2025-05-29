6d2e0361296db12b7973a3b052f23111791a9b0a
gemdev111 <iGemDev@gemwallet.com>
2025-05-29 16:55:30 +0300

    minor updates validators

--- Packages/PrimitivesComponents/Sources/Validators/MaximumValueValidaror.swift
+++ Packages/PrimitivesComponents/Sources/Validators/MaximumValueValidaror.swift
@@ -5,22 +5,21 @@ import Primitives
 
 // TODO: - localize when implement e.g buy, sell
 
-public struct MaximumValueValidator<Value>: ValueValidator
-where Value: ValueValidatable
+public struct MaximumValueValidator<V>: ValueValidator where V: ValueValidatable
 {
-    private let maximumValue: Value
+    private let maximumValue: V
     private let maximumValueText: String
 
-    public init(maximumValue: Value, maximumValueText: String) {
+    public init(maximumValue: V, maximumValueText: String) {
         self.maximumValue = maximumValue
         self.maximumValueText = maximumValueText
     }
 
-    public func validate(_ value: Value) throws {
+    public func validate(_ value: V) throws {
         guard value <= maximumValue else {
             throw AnyError("Maximum allowed value is \(maximumValueText)")
         }
     }
 
-    public var id: String { "MaximumValueValidator<\(Value.self)>" }
+    public var id: String { "MaximumValueValidator<\(V.self)>" }
 }
--- Packages/PrimitivesComponents/Sources/Validators/PositiveValueValidator.swift
+++ Packages/PrimitivesComponents/Sources/Validators/PositiveValueValidator.swift
@@ -3,16 +3,15 @@
 import Foundation
 import Primitives
 
-public struct PositiveValueValidator<Value>: ValueValidator
-where Value: ValueValidatable
+public struct PositiveValueValidator<V>: ValueValidator where V: ValueValidatable
 {
     public init() {}
 
-    public func validate(_ value: Value) throws {
+    public func validate(_ value: V) throws {
         guard value > 0 else {
             throw TransferError.invalidAmount
         }
     }
 
-    public var id: String { "PositiveValidator<\(Value.self)>" }
+    public var id: String { "PositiveValidator<\(V.self)>" }
 }
