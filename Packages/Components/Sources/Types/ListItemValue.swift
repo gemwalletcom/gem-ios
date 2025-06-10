8d5817c0a3f9481e3d5d9231b17a2662c23a3ae2
Ferman <119309671+0xh3rman@users.noreply.github.com>
2025-06-11 00:56:57 +0900

    import SwiftUI instead of SwiftUICore (#783)

--- Packages/Components/Sources/Types/ListItemValue.swift
+++ Packages/Components/Sources/Types/ListItemValue.swift
@@ -1,7 +1,6 @@
 // Copyright (c). Gem Wallet. All rights reserved.
 
 import Foundation
-import SwiftUICore
 import SwiftUI
 
 public struct ListItemValue<T: Identifiable> {
--- Packages/PrimitivesComponents/Sources/ViewModels/AssetTagsViewModel.swift
+++ Packages/PrimitivesComponents/Sources/ViewModels/AssetTagsViewModel.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 import Primitives
-import SwiftUICore
+import SwiftUI
 import Style
 
 public struct AssetTagsViewModel {
