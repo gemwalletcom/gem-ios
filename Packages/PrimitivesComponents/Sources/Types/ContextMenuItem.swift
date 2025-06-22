65d41057b2956f603ddffbd319e5636d58d108ab
gemdev111 <iGemDev@gemwallet.com>
2025-06-22 23:16:38 +0300

    Updated tint

--- Packages/PrimitivesComponents/Sources/Types/ContextMenuItem.swift
+++ Packages/PrimitivesComponents/Sources/Types/ContextMenuItem.swift
@@ -8,29 +8,30 @@ public struct ContextMenuItem: View {
     private let systemImage: String?
     private let role: ButtonRole?
     private let action: () -> Void
+    private let defaultColor: Color
 
     public init(
         title: String,
         systemImage: String? = nil,
         role: ButtonRole? = nil,
+        defaultColor: Color = Colors.black,
         action: @escaping () -> Void
     ) {
         self.title = title
         self.systemImage = systemImage
         self.role = role
+        self.defaultColor = defaultColor
         self.action = action
     }
 
     public var body: some View {
-        Button(role: role) {
-            action()
-        } label: {
+        Button(role: role, action: action) {
             if let systemImage {
                 Label(title, systemImage: systemImage)
             } else {
                 Text(title)
             }
         }
-        .tint(Colors.gray)
+        .tint(role == .destructive ? Color.red : Colors.black)
     }
 }
