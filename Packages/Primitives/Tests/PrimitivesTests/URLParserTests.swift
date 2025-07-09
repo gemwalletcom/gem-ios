5d538ebc074315450a4e90f6c1b6f63c85a4f279
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-09 22:13:08 +0900

    Update URLParserTests.swift

--- Packages/Primitives/Tests/PrimitivesTests/URLParserTests.swift
+++ Packages/Primitives/Tests/PrimitivesTests/URLParserTests.swift
@@ -1,22 +1,21 @@
 // Copyright (c). Gem Wallet. All rights reserved.
 
-import Testing
 import Foundation
 @testable import Primitives
+import Testing
 
 struct URLParserTests {
-
-    @Test func testAssetUrl() async throws {
+    @Test func assetUrl() async throws {
         let chainAction = try URLParser.from(url: URL(string: "https://gemwallet.com/tokens/bitcoin")!)
-        
+
         #expect(chainAction == URLAction.asset(AssetId(chain: .bitcoin, tokenId: .none)))
-        
+
         let tokenAction = try URLParser.from(url: URL(string: "https://gemwallet.com/tokens/ethereum/0xdAC17F958D2ee523a2206206994597C13D831ec7")!)
-        
-        #expect(tokenAction == URLAction.asset(AssetId(chain: .ethereum, tokenId: "0xdAC17F958D2ee523a2206206994597C13D831ec7")) )
+
+        #expect(tokenAction == URLAction.asset(AssetId(chain: .ethereum, tokenId: "0xdAC17F958D2ee523a2206206994597C13D831ec7")))
     }
 
-    @Test func testWCUrl() async throws {
+    @Test func walletConnectSessionTopicUrl() async throws {
         let url = "gem://wc?sessionTopic=64a4f0817e3dd003cbe23202fb6ffaa16d38074de84762a5797e6092b2250a27"
 
         let action = try URLParser.from(url: URL(string: url)!)
