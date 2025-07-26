07e069dbe9034f781f2c8c9425c895c03162f6ad
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-27 00:36:39 +0900

    add exchange endpoint

--- Packages/Blockchain/Sources/Hypercore/HyperCoreProvider.swift
+++ Packages/Blockchain/Sources/Hypercore/HyperCoreProvider.swift
@@ -4,23 +4,28 @@ import Foundation
 import SwiftHTTPClient
 
 public enum HypercoreProvider: TargetType {
-    
     case clearinghouseState(user: String)
     case metaAndAssetCtxs
     case candleSnapshot(coin: String, interval: String, startTime: Int, endTime: Int)
-    
+    case exchange(action: String, signature: String, nonce: UInt64)
+
     public var baseUrl: URL {
         return URL(string: "")!
     }
-    
+
     public var method: HTTPMethod {
         return .POST
     }
-    
+
     public var path: String {
-        return "/info"
+        switch self {
+        case .clearinghouseState, .metaAndAssetCtxs, .candleSnapshot:
+            return "/info"
+        case .exchange:
+            return "/exchange"
+        }
     }
-    
+
     public var data: RequestData {
         switch self {
         case .clearinghouseState(let user):
@@ -42,9 +47,18 @@ public enum HypercoreProvider: TargetType {
                     "endTime": .integer(endTime)
                 ])
             ]))
+        case .exchange(let action, let signature, let nonce):
+            let action = try! JSONSerialization.jsonObject(with: action.data(using: .utf8)!, options: [])
+            let object: [String: Any] = [
+                "action": action,
+                "signature": signature,
+                "nonce": nonce
+            ]
+            let data = try! JSONSerialization.data(withJSONObject: object, options: [])
+            return .data(data)
         }
     }
-    
+
     public var contentType: String {
         ContentType.json.rawValue
     }
