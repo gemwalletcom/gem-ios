09d9d8387182ecf86439011cdee16be7562eea78
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-29 20:55:07 +0900

    Refactor HyperCoreSigner and remove SignatureParser
    Refactored HyperCoreSigner to use new typed data methods and centralized signed request building in the factory. Removed the now-unused SignatureParser. Added a utility to get the current timestamp in milliseconds to Date extensions.

--- Packages/Signer/Sources/Chains/HyperCoreSigner.swift
+++ Packages/Signer/Sources/Chains/HyperCoreSigner.swift
@@ -1,37 +1,36 @@
 // Copyright (c). Gem Wallet. All rights reserved.
 
 import BigInt
+import Blockchain
 import Foundation
 import Gemstone
 import GemstonePrimitives
+import Keychain
 import Keystore
 import Primitives
 import WalletCore
 import WalletCorePrimitives
-import Keychain
-import Blockchain
 
 public class HyperCoreSigner: Signable {
-    
     static let keychain = KeychainDefault()
     let hyperCore = HyperCore()
     let factory = HyperCoreModelFactory()
-    
+
     public func signTransfer(input: SignerInput, privateKey: Data) throws -> String {
         fatalError()
     }
-    
+
     func getAgentKey() throws -> (address: String, key: Data) {
         if let key = try Self.keychain.get(HyperCoreService.HLAgentKey), let address = try Self.keychain.get(HyperCoreService.HLAgentAddress) {
-            return (address: address, try Data.from(hex: key))
+            return try (address: address, Data.from(hex: key))
         }
         let newKeyHex = try SecureRandom.generateKey()
         let newKey = try Data.from(hex: newKeyHex)
         let newAddress = CoinType.ethereum.deriveAddress(privateKey: PrivateKey(data: newKey)!)
-        
+
         try Self.keychain.set(newKeyHex, key: HyperCoreService.HLAgentKey)
         try Self.keychain.set(newAddress, key: HyperCoreService.HLAgentAddress)
-        
+
         return (address: newAddress, key: newKey)
     }
 
@@ -40,26 +39,36 @@ public class HyperCoreSigner: Signable {
             throw AnyError("Invalid input type for perpetual signing")
         }
         NSLog("asset \(asset), type \(type)")
-        
+
         let (agentAddress, agentKey) = try getAgentKey()
-        let timestamp = UInt64(Date().timeIntervalSince1970 * 1000)
-        
+        let timestamp = Date.getTimestampInMs()
+
         // simple check for now, passed from hypercore service.
         if input.sequence > 0 {
-            return [
-                try self.signMarketMessage(type: type, agentKey: agentKey),
+            return try [
+                signMarketMessage(type: type, agentKey: agentKey),
             ]
         } else {
             let agentName = "gemwallet_" + agentAddress.suffix(6)
             let agent = factory.makeApproveAgent(name: agentName, address: agentAddress, nonce: timestamp)
-            let eip712Message = hyperCore.encodeApproveAgent(agent: agent)
+            let eip712Message = hyperCore.approveAgentTypedData(agent: agent)
             let signature = try signEIP712(messageJson: eip712Message, privateKey: privateKey)
-            
-            return [
-                try self.actionMessage(signature: signature, eip712Message: eip712Message, timestamp: timestamp),
-                try self.signMarketMessage(type: type, agentKey: agentKey),
+
+            return try [
+                actionMessage(signature: signature, eip712Message: eip712Message, timestamp: timestamp),
+                signMarketMessage(type: type, agentKey: agentKey),
             ]
         }
+    }
+
+    public func signWithdraw(input: SignerInput, privateKey: Data) throws -> String {
+        // FIXME: make sure input.amount is correct  ("2" means 2 USD)
+        let timestamp = Date.getTimestampInMs()
+        let request = factory.makeWithdraw(amount: input.value.description, address: input.senderAddress.lowercased(), nonce: timestamp)
+        let eip712Message = hyperCore.withdrawalRequestTypedData(request: request)
+        let signature = try signEIP712(messageJson: eip712Message, privateKey: privateKey)
+
+        return try actionMessage(signature: signature, eip712Message: eip712Message, timestamp: timestamp)
     }
 
     private func signEIP712(messageJson: String, privateKey: Data) throws -> String {
@@ -69,47 +78,35 @@ public class HyperCoreSigner: Signable {
         }
         return signature.hexString.append0x
     }
-    
+
     private func signMarketMessage(type: PerpetualType, agentKey: Data) throws -> String {
-        let timestamp = UInt64(Date().timeIntervalSince1970 * 1000)
-        
+        let timestamp = Date.getTimestampInMs()
+
         let signature: String
-        let eip712Message: String
+        let actionJson: String
         switch type {
         case .close(let asset, let price, let size):
             let order = factory.makeMarketClose(asset: asset, price: price, size: size, reduceOnly: true)
-            eip712Message = hyperCore.encodePlaceOrder(order: order, nonce: timestamp)
+            let eip712Message = hyperCore.placeOrderTypedData(order: order, nonce: timestamp)
             signature = try signEIP712(messageJson: eip712Message, privateKey: agentKey)
+            actionJson = factory.serializeOrder(order: order)
+
         case .open(let direction, let asset, let price, let size):
             let isBuy = switch direction {
             case .long: true
             case .short: false
             }
             let order = factory.makeMarketOpen(asset: asset, isBuy: isBuy, price: price, size: size, reduceOnly: true)
-            eip712Message = hyperCore.encodePlaceOrder(order: order, nonce: timestamp)
+            let eip712Message = hyperCore.placeOrderTypedData(order: order, nonce: timestamp)
             signature = try signEIP712(messageJson: eip712Message, privateKey: agentKey)
-//        case .withdraw:
-//            // FIXME: make sure input.amount is correct  ("2" means 2 USD)
-//            let request = factory.makeWithdraw(amount: input.value.description, address: input.senderAddress.lowercased(), nonce: timestamp)
-//            let eip712_json = hyperCore.encodeWithdrawalRequest(request: request)
-//            signature = try signEIP712(messageJson: eip712_json, privateKey: privateKey)
+            actionJson = factory.serializeOrder(order: order)
         }
-        return try actionMessage(signature: signature, eip712Message: eip712Message, timestamp: timestamp)
+        return factory.buildSignedRequest(signature: signature, action: actionJson, timestamp: timestamp)
     }
-    
+
     private func actionMessage(signature: String, eip712Message: String, timestamp: UInt64) throws -> String {
         let eip712Json = try JSONSerialization.jsonObject(with: eip712Message.data(using: .utf8)!) as! [String: Any]
-        let (r, s, v) = try SignatureParser.parseSignature(signature)
-        let dictionary: [String: Any] = [
-            "action": eip712Json["message"]!,
-            "signature": [
-                "r": r,
-                "s": s,
-                "v": v,
-            ],
-            "nonce": timestamp,
-            "isFrontend": true,
-        ]
-        return try JSONSerialization.data(withJSONObject: dictionary, options: [.sortedKeys]).encodeString()
+        let actionJson = try JSONSerialization.data(withJSONObject: eip712Json["message"]!).encodeString()
+        return factory.buildSignedRequest(signature: signature, action: actionJson, timestamp: timestamp)
     }
 }
