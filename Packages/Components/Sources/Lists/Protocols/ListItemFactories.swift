9ca499a09add4742f3be0e39a8847712cb43d146
gemdev111 <iGemDev@gemwallet.com>
2025-08-18 15:35:57 +0300

    removed unused import

--- Packages/Components/Sources/Lists/Protocols/ListItemFactories.swift
+++ Packages/Components/Sources/Lists/Protocols/ListItemFactories.swift
@@ -1,7 +1,6 @@
 // Copyright (c). Gem Wallet. All rights reserved.
 
 import Foundation
-import Primitives
 
 public protocol CommonListItemFactory: ListItemRepresentable {
     static func common(_ item: CommonListItem) -> Self
