835b9880533da469962449dae249403bee2bf6eb
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-09-10 17:26:56 +0000

    Improve NFT asset update and response decoding
    NFTService.updateAssets now returns the number of updated NFTs and logs the count in CollectionsViewModel. GemAPI endpoint for NFT assets switched from v2 to v1. ResponseResult decoding improved to handle null data and errors more robustly, and ResponseError properties made public.

--- Packages/Primitives/Sources/Response.swift
+++ Packages/Primitives/Sources/Response.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public struct ResponseError: Codable, Sendable {
-    let error: ResponseMessage
-    
+    public let error: ResponseMessage
+
     public struct ResponseMessage: Codable, Sendable, Error {
         public let message: String
     }
@@ -11,16 +11,36 @@ public struct ResponseError: Codable, Sendable {
 public struct ResponseResult<T: Codable & Sendable>: Codable, Sendable {
     public let data: T
 
-    public init(_ data: T) { self.data = data }
+    public init(_ data: T) {
+        self.data = data
+    }
 
     public init(from decoder: Decoder) throws {
         if let error = try? ResponseError(from: decoder) {
             throw error.error
         }
-        self.data = try T(from: decoder)
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        if try container.decodeNil(forKey: .data) {
+            throw DecodingError.valueNotFound(
+                T.self,
+                .init(
+                    codingPath: [CodingKeys.data],
+                    debugDescription: "Expected non-null data but found null."
+                )
+            )
+        }
+
+        self.data = try container.decode(T.self, forKey: .data)
     }
 
     public func encode(to encoder: Encoder) throws {
-        try data.encode(to: encoder)
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(data, forKey: .data)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case data
     }
 }
