5742a708326c2b4acceb3fe13c372a238d9153a8
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-10-16 13:24:46 +0500

    Layout fixes

--- Features/Assets/Sources/Scenes/AssetScene.swift
+++ Features/Assets/Sources/Scenes/AssetScene.swift
@@ -165,6 +165,7 @@ public struct AssetScene: View {
         .taskOnce(model.fetchOnce)
         .listSectionSpacing(.compact)
         .navigationTitle(model.title)
+        .contentMargins([.top], .small, for: .scrollContent)
     }
 }
 
