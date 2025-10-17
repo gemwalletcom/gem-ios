7bbe979ee58756682e4357b86b97bfd8faf5ede4
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-10-17 17:22:04 +0000

    Move TransferDataType.asset to GemstonePrimitives extension
    Relocates the asset computed property from TransferDataType in Primitives to a new extension in GemstonePrimitives. The new implementation also adds support for the .transferNft case, returning the associated asset instead of triggering a fatalError.

--- Packages/Primitives/Sources/TransferDataType.swift
+++ Packages/Primitives/Sources/TransferDataType.swift
@@ -135,21 +135,4 @@ public enum TransferDataType: Hashable, Equatable, Sendable {
         case .transfer, .deposit, .withdrawal, .swap, .generic: false
         }
     }
-
-    public var asset: Asset {
-        switch self {
-        case .transfer(let asset),
-             .deposit(let asset),
-             .withdrawal(let asset),
-             .swap(let asset, _, _),
-             .stake(let asset, _),
-             .account(let asset, _),
-             .perpetual(let asset, _),
-             .tokenApprove(let asset, _),
-             .generic(let asset, _, _):
-            return asset
-        case .transferNft:
-            fatalError("NFT asset not supported")
-        }
-    }
 }
