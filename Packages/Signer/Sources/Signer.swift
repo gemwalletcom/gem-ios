12ca488cd00e8a1ef77b49b4a7a043c785e43d39
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-10-31 08:50:27 +0900

    merge cases

--- Packages/Signer/Sources/Signer.swift
+++ Packages/Signer/Sources/Signer.swift
@@ -81,14 +81,13 @@ public struct Signer: Sendable {
         case .tron: TronSigner()
         case .bitcoin: BitcoinSigner()
         case .aptos: AptosSigner()
-        case .sui: ChainSigner(chain: chain)
         case .xrp: XrpSigner()
         case .near: NearSigner()
         case .stellar: StellarSigner()
         case .algorand: AlgorandSigner()
         case .polkadot: PolkadotSigner()
         case .cardano: CardanoSigner()
-        case .hyperCore: ChainSigner(chain: chain)
+        case .sui, .hyperCore: ChainSigner(chain: chain)
         }
     }
 }
