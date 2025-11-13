509befae6b5e5a674325de8b11edbecac529dd07
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-11-13 18:11:44 +0000

    Refactor Chatwoot config to use constants
    Moved Chatwoot website token and base URL to shared constants for better maintainability. Updated SupportSceneViewModel to reference these constants. Also updated core submodule.

--- Packages/Primitives/Sources/Constants.swift
+++ Packages/Primitives/Sources/Constants.swift
@@ -5,4 +5,7 @@ import Foundation
 public struct Constants {
     public static let apiURL = "https://api.gemwallet.com"
     public static let assetsURL = "https://assets.gemwallet.com"
+    
+    public static let chatwootURL = URL(string: "https://support.gemwallet.com")!
+    public static let chatwootPublicToken = "21yu9Az48rJHe1rg4poHqLSr"
 }
