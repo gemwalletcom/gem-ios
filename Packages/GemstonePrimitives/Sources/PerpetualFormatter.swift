d75cb1b7fa60574baedc46960d0c8b96b818917f
gemdev111 <iGemDev@gemwallet.com>
2025-11-18 16:46:15 +0200

    Update PerpetualFormatter

--- Packages/GemstonePrimitives/Sources/PerpetualFormatter.swift
+++ Packages/GemstonePrimitives/Sources/PerpetualFormatter.swift
@@ -5,27 +5,26 @@ import Primitives
 import Gemstone
 
 public struct PerpetualFormatter {
-    private let perpetual = Gemstone.Perpetual()
-    private let provider: Primitives.PerpetualProvider
+    private let perpetual: Gemstone.Perpetual
 
     public init(provider: Primitives.PerpetualProvider) {
-        self.provider = provider
+        self.perpetual = Gemstone.Perpetual(provider: provider.map())
     }
 
     public func minimumOrderUsdAmount(price: Double, decimals: Int32, leverage: UInt8) -> UInt64 {
-        perpetual.minimumOrderUsdAmount(provider: provider.map(), price: price, decimals: decimals, leverage: leverage)
+        perpetual.minimumOrderUsdAmount(price: price, decimals: decimals, leverage: leverage)
     }
 
     public func formatPrice(_ price: Double, decimals: Int32) -> String {
-        perpetual.formatPrice(provider: provider.map(), price: price, decimals: decimals)
+        perpetual.formatPrice(price: price, decimals: decimals)
     }
 
     public func formatSize(_ size: Double, decimals: Int32) -> String {
-        perpetual.formatSize(provider: provider.map(), size: size, decimals: decimals)
+        perpetual.formatSize(size: size, decimals: decimals)
     }
 
     public func formatInputPrice(_ price: Double, decimals: Int32, locale: Locale = .current) -> String {
-        let formatted = perpetual.formatPrice(provider: provider.map(), price: price, decimals: decimals)
+        let formatted = perpetual.formatPrice(price: price, decimals: decimals)
         let decimalSeparator = locale.decimalSeparator ?? "."
         guard decimalSeparator != "." else {
             return formatted
