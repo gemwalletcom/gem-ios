3aafb37cab7451d2e1fbf0241539c62ade0d967c
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-12-08 10:23:04 +0500

    Add WebSocketClient package and refactor PriceObserverService (#1468)
    * Add WebSocketClient package and refactor PriceObserverService
    
    * Move WebSocketClient into SwiftHTTPClient package
    
    * Move WebSocketClient to separate target in SwiftHTTPClient package

---
+++ Packages/SwiftHTTPClient/WebSocketClient/TestKit/WebSocketConnectionMock.swift
@@ -0,0 +1,74 @@
+// Copyright (c). Gem Wallet. All rights reserved.
+
+import Foundation
+import WebSocketClient
+
+public actor WebSocketConnectionMock: WebSocketConnectable {
+
+    private var continuation: AsyncStream<WebSocketEvent>.Continuation?
+    private var sentData: [Data] = []
+
+    public private(set) var state: WebSocketState = .disconnected
+
+    public init() {}
+
+    // MARK: - WebSocketConnectable
+
+    public func connect() -> AsyncStream<WebSocketEvent> {
+        AsyncStream { [weak self] continuation in
+            guard let self else {
+                continuation.finish()
+                return
+            }
+
+            Task {
+                await self.setupMockStream(continuation)
+            }
+        }
+    }
+
+    public func disconnect() async {
+        state = .disconnected
+        continuation?.yield(.disconnected(nil))
+        continuation?.finish()
+        continuation = nil
+    }
+
+    public func send(_ data: Data) async throws {
+        guard state == .connected else {
+            throw WebSocketError.notConnected
+        }
+        sentData.append(data)
+    }
+
+    // MARK: - Mock Control API
+
+    public func simulateConnected() {
+        state = .connected
+        continuation?.yield(.connected)
+    }
+
+    public func simulateMessage(_ data: Data) {
+        continuation?.yield(.message(data))
+    }
+
+    public func simulateDisconnect(error: Error? = nil) {
+        state = .disconnected
+        continuation?.yield(.disconnected(error))
+    }
+
+    public func getSentData() -> [Data] {
+        sentData
+    }
+
+    public func clearSentData() {
+        sentData.removeAll()
+    }
+
+    // MARK: - Private
+
+    private func setupMockStream(_ continuation: AsyncStream<WebSocketEvent>.Continuation) {
+        self.continuation = continuation
+        state = .connecting
+    }
+}
