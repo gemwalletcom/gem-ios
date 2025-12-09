87573d4df6bf07a2c5f25d3e4d93873b7235ecb1
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-12-09 21:20:05 +0500

    Refactor AppDelegate launch logic

--- Packages/FeatureServices/AppService/OnstartService.swift
+++ Packages/FeatureServices/AppService/OnstartService.swift
@@ -7,17 +7,18 @@ import NodeService
 import AssetsService
 import Preferences
 import WalletService
+import UIKit
 
 // OnstartService runs services before the app starts.
 // See OnstartAsyncService for any background tasks to run after start
 public struct OnstartService: Sendable {
-    
+
     private let assetsService: AssetsService
     private let assetStore: AssetStore
     private let nodeStore: NodeStore
     private let preferences: Preferences
     private let walletService: WalletService
-    
+
     public init(
         assetsService: AssetsService,
         assetStore: AssetStore,
@@ -32,7 +33,25 @@ public struct OnstartService: Sendable {
         self.walletService = walletService
     }
 
-    public func migrations() {
+    @MainActor
+    public func configure() {
+        validateDeviceSecurity()
+        configureURLCache()
+        excludeDirectoriesFromBackup()
+        migrations()
+        preferences.incrementLaunchesCount()
+
+        #if DEBUG
+        configureScreenshots()
+        #endif
+    }
+}
+
+// MARK: - Private
+
+extension OnstartService {
+
+    private func migrations() {
         do {
             try walletService.setup(chains: AssetConfiguration.allChains)
         } catch {
@@ -47,9 +66,45 @@ public struct OnstartService: Sendable {
         } catch {
             debugLog("migrations error: \(error)")
         }
-        
+
         if !preferences.hasCurrency, let currency = Locale.current.currency {
             preferences.currency = (Currency(rawValue: currency.identifier) ?? .usd).rawValue
+        }
+    }
+
+    private func configureURLCache() {
+        URLCache.shared.memoryCapacity = 256_000_000 // ~256 MB memory space
+        URLCache.shared.diskCapacity = 1_000_000_000 // ~1GB disk cache space
+    }
+
+    private func excludeDirectoriesFromBackup() {
+        do {
+            let excludedBackupDirectories: [FileManager.Directory] = [.documents, .applicationSupport, .library(.preferences)]
+            try excludedBackupDirectories.forEach {
+                try FileManager.default.addSkipBackupAttributeToItemAtURL($0.url)
+
+                #if DEBUG
+                debugLog("Excluded backup directory: \($0.directory)")
+                #endif
+            }
+        } catch {
+            debugLog("addSkipBackupAttributeToItemAtURL error \(error)")
+        }
+    }
+
+    @MainActor
+    private func validateDeviceSecurity() {
+        let device = UIDevice.current
+        if !device.isSimulator && (device.isJailBroken || device.isFridaDetected) {
+            fatalError()
+        }
+    }
+
+    private func configureScreenshots() {
+        if ProcessInfo.processInfo.environment["SCREENSHOTS_PATH"] != nil {
+            if let currency = Locale.current.currency {
+                Preferences.standard.currency = currency.identifier
+            }
         }
     }
 }
