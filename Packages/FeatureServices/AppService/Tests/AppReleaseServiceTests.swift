d85b469fe016c41b5e76131a7828e60672af51f3
Radmir <52320354+DRadmir@users.noreply.github.com>
2025-12-11 15:33:33 +0500

    Add AppServiceTests to test plan and fix test dependencies

--- Packages/FeatureServices/AppService/Tests/AppReleaseServiceTests.swift
+++ Packages/FeatureServices/AppService/Tests/AppReleaseServiceTests.swift
@@ -2,7 +2,11 @@
 
 import Foundation
 import Testing
+import Primitives
+import PrimitivesTestKit
+import GemAPI
 import GemAPITestKit
+@testable import AppService
 
 struct AppReleaseServiceTests {
     @Test
@@ -10,10 +14,10 @@ struct AppReleaseServiceTests {
         let service = AppReleaseService(configService: GemAPIConfigServiceMock(config: .mock()))
         #expect(try await service.getNewestRelease()?.version == "16.1")
     }
-    
+
     @Test
     func testReleaseFromConfig() async throws {
         let service = AppReleaseService(configService: GemAPIConfigServiceMock(config: .mock()))
-        #expect(service.release(.mock())?.version == "16.1")
+        #expect(service.release(ConfigResponse.mock())?.version == "16.1")
     }
 }
