5d1ef8e4189fc5a86b607cc38d091fc1a79d4538
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-01-13 00:39:00 +0200

    Update PinnedSectionHeader.swift

--- Packages/PrimitivesComponents/Sources/Views/PinnedSectionHeader.swift
+++ Packages/PrimitivesComponents/Sources/Views/PinnedSectionHeader.swift
@@ -5,15 +5,12 @@ import Style
 import Localization
 
 public struct PinnedSectionHeader: View {
-    public static var title: String { Localized.Common.pinned }
-    public static var image: Image { Images.System.pin }
-
     public init() {}
 
     public var body: some View {
         SectionHeaderView(
-            title: Self.title,
-            image: Self.image
+            title: Localized.Common.pinned,
+            image: Images.System.pin
         )
     }
 }
